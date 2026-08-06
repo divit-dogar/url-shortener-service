f8881181cf7caa4f44b48ee5f5ab7d3801a45719
Dayanand <divitdogar@gmail.com>
2026-08-06 13:25:41 +0530

    Export schema package

---
+++ app/schemas/__init__.py
@@ -0,0 +1,45 @@
+"""
+Schema Package
+
+Exports all schemas used across the application.
+"""
+
+# User Schemas
+from .user import (
+    UserCreate,
+    UserResponse,
+    UserUpdate,
+)
+
+# URL Schemas
+from .url import (
+    URLCreate,
+    URLResponse,
+    URLStats,
+    URLUpdate,
+)
+
+# Authentication Schemas
+from .auth import (
+    LoginRequest,
+    Token,
+    TokenPayload,
+)
+
+__all__ = [
+    # User
+    "UserCreate",
+    "UserResponse",
+    "UserUpdate",
+
+    # URL
+    "URLCreate",
+    "URLResponse",
+    "URLStats",
+    "URLUpdate",
+
+    # Auth
+    "LoginRequest",
+    "Token",
+    "TokenPayload",
+]
