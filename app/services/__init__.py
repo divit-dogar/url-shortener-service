ca89da0d36d4c4f3ef767d901be6cb85d7859ff7
Dayanand <divitdogar@gmail.com>
2026-08-07 11:53:16 +0530

    Export dashboard service

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -7,9 +7,11 @@ Exports all services.
 from .analytics_service import AnalyticsService
 from .auth_service import AuthService
 from .url_service import URLService
+from .dashboard_service import DashboardService
 
 __all__ = [
     "AuthService",
     "URLService",
     "AnalyticsService",
+    "DashboardService",
 ]
