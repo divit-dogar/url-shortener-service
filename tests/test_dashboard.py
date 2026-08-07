e11ba30ee113181cdb49110bdabecf56f5597077
Dayanand <divitdogar@gmail.com>
2026-08-07 14:33:44 +0530

    Add dashboard tests

---
+++ tests/test_dashboard.py
@@ -0,0 +1,23 @@
+from .conftest import client
+
+
+def test_dashboard():
+
+    login = client.post(
+        "/auth/login",
+        data={
+            "username": "test@example.com",
+            "password": "Password@123",
+        },
+    )
+
+    token = login.json()["access_token"]
+
+    response = client.get(
+        "/dashboard",
+        headers={
+            "Authorization": f"Bearer {token}"
+        },
+    )
+
+    assert response.status_code == 200
