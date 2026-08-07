561f6adb5df62aa7e89698afa9e87d6b1f04a384
Dayanand <divitdogar@gmail.com>
2026-08-07 08:58:58 +0530

    Initialize application and creted database table

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+
 """
 Main Application
 
@@ -5,8 +6,13 @@ Entry point of the URL Shortener Service.
 """
 
 from fastapi import FastAPI
-
+import app.models
+from app.core.database import Base, engine
 from app.api.auth import router as auth_router
+
+
+## Create database tables
+Base.metadata.create_all(bind=engine)
 
 # Create FastAPI application
 app = FastAPI(
@@ -18,9 +24,29 @@ app = FastAPI(
 # Health Check
 @app.get("/")
 def root():
-    """
-    Root endpoint to verify the application is running.
-    """
+   # Root endpoint to verify the application is running.
+  
+    return {
+        "message": "URL Shortener Service is running 🚀"
+    }
+
+# Register Routers
+app.include_router(auth_router)
+
+
+# Create FastAPI application
+app = FastAPI(
+    title="URL Shortener Service",
+    description="A URL shortening service built with FastAPI",
+    version="1.0.0",
+)
+
+# Health Check
+@app.get("/")
+def root():
+    
+    # Root endpoint to verify the application is running.
+    
     return {
         "message": "URL Shortener Service is running 🚀"
     }
